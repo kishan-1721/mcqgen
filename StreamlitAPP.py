b44e5558c7b02637f5125a1b13b3e898bb958730
kishan-1721 <kishanpatel9063.com>
2024-01-19 11:12:35 +0530

    MCQGenerator

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -15,14 +15,55 @@ from langchain_community.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chains import SequentialChain
+from langchain_community.callbacks import get_openai_callback
 
 #loading json file
-with open("C:\Users\kisha\Projects\Generative_AI\mcqgen\Response.json", 'r') as file:
+with open(r"C:\Users\kisha\Projects\Generative_AI\mcqgen\Response.json", 'r') as file:
     RESPONSE_JSON = json.load(file)
 
 #Creating Title for the App
 st.title("MCQ Creator Application with Langchain")
 
-# #Create a form using st.form
-# with st.form('user_inputs'):
-#     #File Upload
+
+# Create a form using st.form
+with st.form("user_inputs"):
+    # File Uploader
+    uploaded_file = st.file_uploader("Upload a PDF or txt file")
+
+    # Input Fields
+    mcq_count = st.number_input("No. of MCQs", min_value=3, max_value=50)
+    subject = st.text_input("Insert Subject", max_chars=20)
+    tone = st.text_input("Complexity Level of Questions", max_chars=20, placeholder="Simple")
+
+    # Button
+    button = st.form_submit_button("Create MCQs")
+
+    # Check if the button is clicked and all fields have input
+    if button and uploaded_file is not None and mcq_count and subject and tone:
+        with st.spinner("Loading..."):
+            try:
+                # Read file
+                text = read_file(uploaded_file)
+                #Count Tokens and the cost of API call
+                #How to setup Token Usage Tracking in LangChain
+                
+                with get_openai_callback() as cb:
+                    response=generate_evaluate_chain(
+                        {
+                            "text": text,
+                            "number": mcq_count,
+                            "subject":subject,
+                            "tone": tone,
+                            "response_json": json.dumps(RESPONSE_JSON)
+                        }
+                        )
+
+
+                # Display success message or result
+                # st.success("MCQs created successfully!")
+
+            except Exception as e:
+                traceback.print_exception(type(e), e, e.__traceback__)
+                st.error("Error")
+
+
